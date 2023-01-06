2479a498fe0e99083f8c428c91214c4cbffcd0a9
Callum Gill <callum.gill.1993.cg@gmail.com>
2023-01-06 08:23:27 -0800

    Added a cars table

--- resources/views/pages/cars.blade.php
+++ resources/views/pages/cars.blade.php
@@ -1,8 +1,25 @@
 @extends('layouts.default')
 
 @section('content')
-    <h1>Cars</h1>
-    @foreach($cars as $car)
-        <span>{{ $car->make }}</span>
-    @endforeach
+    <h1 class="center">Cars</h1>
+    <table class="table-auto w-full">
+        <thead>
+        <tr class="bg-gray-800 text-white">
+            <th class="px-4 py-2">Make</th>
+            <th class="px-4 py-2">Model</th>
+            <th class="px-4 py-2">License</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($cars as $car)
+            <tr class="bg-gray-100">
+                <td class="px-4 py-2">{{ $car->make }}</td>
+                <td class="px-4 py-2">{{ $car->model }}</td>
+                <td class="px-4 py-2">{{ $car->model }}</td>
+            </tr>
+
+        @endforeach
+        </tbody>
+    </table>
+
 @endsection

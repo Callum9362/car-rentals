120e90f55a13c0282a8620e3caceb4c80be8651d
Callum Gill <callum.gill.1993.cg@gmail.com>
2023-01-06 09:24:47 -0800

    Added the login and logged out functionality

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 
-    <div class="container mx-auto w-25">
-        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="POST" action="{{ route('login') }}">
+    <div class="flex items-center justify-center mt-10">
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/2" method="POST" action="{{ route('login') }}">
             @csrf
             <div class="mb-4">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
                     Email
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('email') outline-red-500 @enderror" id="email" type="email" placeholder="Email">
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('email') outline-red-500 @enderror" name="email" type="email" placeholder="Email">
                 @error('email')
                 <p class="text-red-500 text-xs italic">{{ $message }}</p>
                 @enderror
@@ -18,12 +18,12 @@
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
                     Password
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('password') outline-red-500 @enderror" id="password" type="password" placeholder="********">
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('password') outline-red-500 @enderror" name="password" type="password" placeholder="********">
                 @error('password')
                 <p class="text-red-500 text-xs italic">{{ $message }}</p>
                 @enderror
             </div>
-            <div class="form-check mb-6">
+            <div class="form-check mb-6 mt-1">
                 <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                 <label class="form-check-label" for="remember">
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -6,10 +6,18 @@
             </a>
         </div>
         <div class="w-3/4 flex items-center justify-end">
-            <a href="{{ route('cars') }}" class="text-gray-400 hover:text-white font-bold px-4">Cars</a>
-            <a href="{{ route('rentals') }}" class="text-gray-400 hover:text-white font-bold px-4">Rentals</a>
-            <a href="#" class="text-gray-400 hover:text-white font-bold px-4">Locations</a>
-            <button class="bg-blue-500 text-white rounded-full px-4 py-2 focus:outline-none hover:bg-blue-600">Sign up</button>
+            @auth
+                <a href="{{ route('cars') }}" class="text-gray-400 hover:text-white font-bold px-4">Cars</a>
+                <a href="{{ route('rentals') }}" class="text-gray-400 hover:text-white font-bold px-4">Rentals</a>
+                <a href="#" class="text-gray-400 hover:text-white font-bold px-4">Locations</a>
+                <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <button class="text-gray-400 hover:text-white font-bold px-4" type="submit">Logout</button>
+                </form>
+            @endauth
+            @guest
+                <a href="{{ route('login') }}" class="text-gray-400 hover:text-white font-bold px-4">Login</a>
+           @endguest
         </div>
     </div>
 </nav>

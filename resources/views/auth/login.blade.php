e9011ccc9b397b6aca20fef842e624e88ab60b1d
Callum Gill <callum.gill.1993.cg@gmail.com>
2023-01-08 15:36:39 -0800

    Added a indigo navbar for a while...

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,7 +31,7 @@
                 </label>
             </div>
             <div class="flex items-center justify-between">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                <button class="bg-indigo-600 hover:text-indigo-300 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                     Sign In
                 </button>
                 <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="{{ route('password.request') }}">
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,22 +1,22 @@
-<nav class="bg-gray-800">
+<nav class="bg-indigo-600">
     <div class="container mx-auto px-4 py-2 flex items-center justify-between">
         <div class="w-1/4">
-            <a class="text-gray-400 hover:text-white font-bold px-4" href="{{ route('index') }}">
+            <a class="text-white hover:text-indigo-300 font-bold px-4" href="{{ route('index') }}">
                 CarRent
             </a>
         </div>
         <div class="w-3/4 flex items-center justify-end">
             @auth
-                <a href="{{ route('cars') }}" class="text-gray-400 hover:text-white font-bold px-4">Cars</a>
-                <a href="{{ route('rentals') }}" class="text-gray-400 hover:text-white font-bold px-4">Rentals</a>
-                <a href="#" class="text-gray-400 hover:text-white font-bold px-4">Locations</a>
+                <a href="{{ route('cars') }}" class="text-white hover:text-indigo-300 font-bold px-4">Cars</a>
+                <a href="{{ route('rentals') }}" class="text-white hover:text-indigo-300 font-bold px-4">Rentals</a>
+                <a href="#" class="text-white hover:text-indigo-300 font-bold px-4">Locations</a>
                 <form action="{{ route('logout') }}" method="POST">
                     @csrf
-                    <button class="text-gray-400 hover:text-white font-bold px-4" type="submit">Logout</button>
+                    <button class="text-white hover:text-indigo-300 font-bold px-4" type="submit">Logout</button>
                 </form>
             @endauth
             @guest
-                <a href="{{ route('login') }}" class="text-gray-400 hover:text-white font-bold px-4">Login</a>
+                <a href="{{ route('login') }}" class="text-white hover:text-indigo-300 font-bold px-4">Login</a>
            @endguest
         </div>
     </div>

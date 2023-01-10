f74c20f352cfef0df3af4935f23c1f019cec0462
Callum Gill <callum.gill.1993.cg@gmail.com>
2023-01-09 16:17:23 -0800

    Messed with some indigo

--- resources/views/livewire/cars-form-component.blade.php
+++ resources/views/livewire/cars-form-component.blade.php
@@ -54,7 +54,7 @@
                 @enderror
             </div>
             <div class="flex items-center justify-between">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                <button class="bg-indigo-600 hover:bg-indigo-400 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                     Add
                 </button>
             </div>

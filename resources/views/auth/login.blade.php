218903216da60dc90d7942c7697651da7fd51131
Callum Gill <callum.gill.1993.cg@gmail.com>
2023-01-29 09:39:17 -0800

    Started to make more of the site mobile responsive

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div class="flex items-center justify-center mt-10">
-        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/2" method="POST" action="{{ route('login') }}">
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 sm:w-1/2 sm:flex-col" method="POST" action="{{ route('login') }}">
             @csrf
             <div class="mb-4">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
@@ -34,10 +34,11 @@
                 <button class="bg-indigo-600 hover:text-indigo-300 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                     Sign In
                 </button>
-                <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="{{ route('password.request') }}">
+                <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800 ml-2" href="{{ route('password.request') }}">
                     Forgot Password?
                 </a>
             </div>
         </form>
     </div>
+
 @endsection
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,11 +1,11 @@
 <nav class="bg-indigo-600">
     <div class="container mx-auto px-4 py-2 flex items-center justify-between">
-        <div class="w-1/4">
+        <div class="w-1/4 md:w-1/4">
             <a class="text-white hover:text-indigo-300 font-bold px-4" href="{{ route('index') }}">
                 CarRent
             </a>
         </div>
-        <div class="w-3/4 flex items-center justify-end">
+        <div class="w-3/4 md:w-3/4 flex items-center justify-end">
             @auth
                 <a href="{{ route('cars') }}" class="text-white hover:text-indigo-300 font-bold px-4">Cars</a>
                 <a href="{{ route('locations') }}" class="text-white hover:text-indigo-300 font-bold px-4">Locations</a>
